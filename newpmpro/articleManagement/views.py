4c1eda87b5131d8e3dd7fad7f271f554b6d7f407
venkatasrikanthmaddela <srikk1309@gmail.com>
2017-11-16 15:28:54 +0530

    admin store front authentication

--- newpmpro/articleManagement/views.py
+++ newpmpro/articleManagement/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.views import View
-from articleManagement.utils import get_article_data
+from logger import logger
+from articleManagement.utils import get_article_data, article_review_check
 from articleManagement.models import Article, Tags
 from django.views import generic
+from newpmpro.utils import get_log_string
 
 
 class ArticlesListPage(generic.ListView):
@@ -22,8 +24,17 @@ def post_article_page(request):
     return render(request, 'articleManagement/postArticle.html')
 
 
-
 def article_page(request):
     article_id = request.GET.get("article-id")
     articles_list = get_article_data(article_id, request)
-    return render(request, 'articleManagement/articlePage.html', {"allArticles": articles_list})
+    is_super_user = False
+    try:
+        if request.user.is_authenticated and request.user.is_superuser:
+            article_review_check(*articles_list)
+            is_super_user = True
+    except Exception as e:
+        logger.error(get_log_string('error: '+str(e)), exc_info=True)
+    return render(request, 'articleManagement/articlePage.html', {"allArticles": articles_list,
+                                                                  "isSuperUser": is_super_user})
+
+
